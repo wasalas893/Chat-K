fbdeee1d07d7de42b9d589c2429fb0679a14fcc7
wasalas <wasalas893@gmail.com>
2020-06-14 10:03:27 +0530

    deploy

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ import mongodb from 'mongodb';
 
 export default {
    "port": 3000,
-   "mongoUrl": "mongodb://localhost:27017/chat-api",
+   "mongoUrl": "mongodb+srv://suna123:suna123@cluster0-dfx9f.mongodb.net/test?retryWrites=true&w=majority",
   // "port": process.env.PORT,
    //"mongoUrl": process.env.MONGODB_URI,
    "bodyLimit": "100kb"
