6a236077dea210191e57fdff4359e8b5bc64a449
wasalas <wasalas893@gmail.com>
2020-06-16 18:00:39 +0530

    create the CreateUserActivity

---
+++ Android/app/src/main/java/com/example/myapplication/CreateUserActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+    //user avatar
+    fun genarateUserAvatar(view: View){
+
+    }
+    //generate color btn
+   fun generateColorClicked(view: View){
+
+   }
+    //create user btn
+    fun createUserClicked(view: View){
+
+    }
+
+}
+
+
